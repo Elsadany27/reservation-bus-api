1ed7964d55d896ae0a1fe9a7202e7ff3876c566b
Youssef Elsadany <youssefhussien211@gmail.com>
2025-07-26 08:05:52 +0300

    ss

--- myproject/settings.py
+++ myproject/settings.py
@@ -97,6 +97,7 @@ if DATABASE_URL:
     import dj_database_url
     DATABASES = {
     'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+    
     }
 else:
     DATABASES = {
