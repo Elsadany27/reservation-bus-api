78369ef15fbcc9263479f329341c7254d2e91948
Youssef Elsadany <youssefhussien211@gmail.com>
2025-07-26 08:05:01 +0300

    settings

--- myproject/settings.py
+++ myproject/settings.py
@@ -96,7 +96,7 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 if DATABASE_URL:
     import dj_database_url
     DATABASES = {
-    'default': dj_database_url.config(default=DATABASE_URL, conn_max_age=600, ssl_require=True)
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
     }
 else:
     DATABASES = {
